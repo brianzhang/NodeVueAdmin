f92475b93018c8b2e6b3ab2c5e86e6e0389b8ce1
brian <brian@xingyuehuyu.com>
2018-09-05 14:30:43 +0800

    update log

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -23,7 +23,6 @@ app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    console.log('request')
     if (req.method == 'OPTIONS') {
         res.send(200); /*让options请求快速返回*/
     }else {
